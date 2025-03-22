74f4d5779ec65a2dfe5b8aaecc26b713463aabc4
gabrielpires <gabrielpires@ibm.com>
2025-03-22 18:10:59 -0300

    adding get posts from the last 5 days filter

--- src/services/getPosts.js
+++ src/services/getPosts.js
@@ -1,9 +1,23 @@
 const { logger } = require('../utils');
 const { PostModel } = require('../models');
+const { Op } = require('sequelize');
 
-const getPosts = () => {
+const getPosts = (filterLast) => {
     logger.info('Pulling all Posts');
-    return PostModel.findAll().catch((err) => {
+    console.log(new Date().getDate() - 5);
+    return PostModel.findAll(
+        filterLast
+            ? {
+                  where: {
+                      createdAt: {
+                          [Op.gte]: new Date().setDate(
+                              new Date().getDate() - 5
+                          ),
+                      },
+                  },
+              }
+            : undefined
+    ).catch((err) => {
         throw new Error(err);
     });
 };
