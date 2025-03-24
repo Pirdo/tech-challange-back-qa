adfa682438b91e4f77546cb4b66445ae0ba9a40a
gabrielpires <gabrielpires@ibm.com>
2025-03-24 11:29:51 -0300

    adding search posts unit test

---
+++ __tests__/posts/searchPosts.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+
+const app = require('../../src/server');
+const { postgresConn } = require('../../src/config');
+
+describe('GET /posts', () => {
+    beforeAll(async () => {
+        await postgresConn.sync();
+    });
+
+    it('should return status 200 with the data from all posts and a message aswell', async () => {
+        const response = await request(app)
+            .get('/posts/search/')
+            .query({ keyword: 'edit' });
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe(
+            'Successfully searched for the posts'
+        );
+    });
+});
