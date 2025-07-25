3691687c83c81ef385860049ba14d7c37c8526f4
Pirdo <100035058+Pirdo@users.noreply.github.com>
2025-07-25 16:47:41 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const { postgresConn } = require('../config');
 const User = postgresConn.define('User', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: {type: DataTypes.STRING, allowNull: false},
-    user: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
     role: { type: DataTypes.INTEGER, allowNull: false },
 });
